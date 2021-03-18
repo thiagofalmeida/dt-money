a946f5ec977a67c36540d2fec912ed9acb88fb08
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-03-18 11:20:36 -0300

    Criado models e forms

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,101 @@
+import { FormEvent, useState } from 'react'
+import Modal from 'react-modal'
+
+import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { api } from '../../services/api'
+
+import { Container, RadioBox, TransactionTypeContainer } from './styles'
+
+interface MewTransactionModalProps {
+  isOpen: boolean
+  onRequestClose: () => void
+}
+
+export function NewTransactionModal({onRequestClose, isOpen}: MewTransactionModalProps) {
+  const [type, setType] = useState('deposit')
+  const [title, setTitle] = useState('')
+  const [category, setCategory] = useState('')
+  const [value, setValue] = useState(0)
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    }
+  
+    api.post('/transactions', data)
+  }
+
+  return (
+    <Modal 
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+
+      <button 
+        type="button" 
+        onClick={onRequestClose} 
+        className="react-modal-close"
+      >
+        <img src={closeImg} alt="Fechar Modal" />
+      </button>
+
+      <Container onSubmit={handleCreateNewTransaction}>
+        <h2>Cadastrar transação</h2>
+
+        <input 
+          placeholder="Título"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
+        />
+
+        <input 
+          placeholder="Valor"
+          type="number"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
+        />
+
+        <TransactionTypeContainer>
+          <RadioBox 
+            type="button"
+            isActive={type=== 'deposit'}
+            activeColor="green"
+            onClick={() => { setType('deposit') }}
+          >
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </RadioBox>
+
+          <RadioBox 
+            type="button"
+            isActive={type=== 'withdraw'}
+            activeColor="red"
+            onClick={() => { setType('withdraw') }}
+          >
+            <img src={outcomeImg} alt="Saída"/>
+            <span>Saída</span>
+          </RadioBox>
+        </TransactionTypeContainer>
+
+        <input 
+          placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
+        />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
+    </Modal>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import { App } from './App';
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Salário',
+          type: 'deposit',
+          category: 'Renda',
+          amount: 5000,
+          createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Aluguel',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 600,
+          createdAt: new Date('2021-02-14 10:00:00')
+        }
+      ]
+    })
+  },
+
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction',
-          amount: 400,
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
     })
   }
 })
