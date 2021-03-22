29dba344bbc9eaa94cab3a76fb362f9b367c700a
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-03-22 10:59:56 -0300

    Criado hook para facilitar a chamada do context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Modal from 'react-modal'
 
-import { TransactionsContext, TransactionsProvider } from './TransactionsContext';
+import { TransactionsProvider } from './hooks/useTransactions';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
--- src/hooks/useTransactions.tsx
+++ src/hooks/useTransactions.tsx
@@ -1,6 +1,6 @@
-import { createContext, useEffect, useState, ReactNode } from "react";
+import { createContext, useEffect, useState, ReactNode, useContext } from "react";
 
-import { api } from "./services/api";
+import { api } from "../services/api";
 
 interface Transaction {
   id: number
@@ -22,7 +22,7 @@ interface TransactionsContextData {
   createTransaction: (transaction: TransactionInput) => Promise<void>
 }
 
-export const TransactionsContext = createContext<TransactionsContextData>(
+const TransactionsContext = createContext<TransactionsContextData>(
   {} as TransactionsContextData
 );
 
@@ -57,4 +57,10 @@ export const TransactionsProvider = ({ children }: TransactionProviderProps) => 
       {children}
     </TransactionsContext.Provider>
   )
+}
+
+export function useTransactions() {
+  const context = useContext(TransactionsContext)
+
+  return context
 }
